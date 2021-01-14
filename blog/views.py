68fca5cf8b4187bace9fc2f33c095aa3329b2db4
Narvik Aghamalian <ideavision@Narviks-MacBook-Pro.local>
2021-01-14 22:34:28 +0400

    improved more templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
+from .models import *
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import *
+
 
 class IndexPage(TemplateView):
     
@@ -15,7 +16,28 @@ class IndexPage(TemplateView):
                 'category' : article.category.title,
                 'created_at' : article.created_at.date()
             })
+
+        promote_data = []
+        all_promote_articles = Article.objects.filter(promote=True)
+        for promote_article in all_promote_articles:
+            promote_data.append({
+                'category' : promote_article.category.title,
+                'title' : promote_article.title,
+                'author' : promote_article.author.user.username, 
+                'avatar' : promote_article.author.avatar.url if promote_article.author.avatar else None,
+                'cover' : promote_article.cover.url if promote_article.cover else None,
+                'created_at' : promote_article.created_at.date(),
+
+            })
+
+
         context= {
             'article_data' : article_data,
+            'promote_article_data' : promote_data,
         }
         return render(request, 'index.html', context)
+
+class ContactPage(TemplateView):
+    template_name='page-contact.html'
+
+
